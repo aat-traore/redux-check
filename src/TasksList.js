5829bb06983b572ff1ebe31a0bd11e85e0a041b2
aat-traore <traoreadjiadiouma@gmail.com>
2022-10-28 11:38:32 +0000

    new redux

---
+++ src/TasksList.js
@@ -0,0 +1,19 @@
+import TaskItem from "./TaskItem";
+import {useSelector}from 'react-redux';
+
+const TasksList = () => {
+  const tasks = useSelector((state)=> state.todo);
+  return( 
+    <>
+      {tasks.map((t) => (
+        <TaskItem
+          task={t}
+          key={t.id}
+          
+        />
+      ))}
+    </>
+  );
+};
+
+export default TasksList;
